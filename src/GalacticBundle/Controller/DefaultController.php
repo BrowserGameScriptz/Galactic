c40093da084d110942b0556eda054e2d952ee6ee
Ryan Jennings <rj@ryanjennings.co.uk>
2016-03-05 12:39:19 -0500

    Latest update. Login reg working grj.

--- src/GalacticBundle/Controller/DefaultController.php
+++ src/GalacticBundle/Controller/DefaultController.php
@@ -9,26 +9,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        die('home');
-    }
-    
-    public function loginAction(Request $request)
-    {
-        $authenticationUtils = $this->get('security.authentication_utils');
-    
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-    
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-    
-        return $this->render(
-            'security/login.html.twig',
-            array(
-                // last username entered by the user
-                'last_username' => $lastUsername,
-                'error'         => $error,
-            )
-        );
+        return $this->render('GalacticBundle:Default:index.html.twig');
     }
 }
